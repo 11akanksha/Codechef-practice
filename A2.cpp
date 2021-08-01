ba371f6df2aa3473ed9a2cc50d4bb7be77eff567
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-01 23:10:57 +0530

    Update A2.cpp

--- A2.cpp
+++ A2.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, d, h;
+        cin >> n >> d >> h;
+        int *a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int wl = 0, f = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > 0)
+            {
+                wl += a[i];
+                if (wl > h)
+                {
+                    cout << "YES" << endl;
+                    f = 1;
+                    break;
+                }
+            }
+            else
+            {
+                if (wl < d)
+                {
+                    wl = 0;
+                }
+                else
+                {
+                    wl -= d;
+                }
+            }
+        }
+        if (f == 0)
+        {
+            cout << "NO" << endl;
+        }
+        delete[] a;
+    }
+    return 0;
+}
